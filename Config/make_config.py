4d34626d97e4c02120b0487a30f5a4f911688a11
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-01-30 20:22:05 +0200

    add get_timetable test

--- Config/make_config.py
+++ Config/make_config.py
@@ -3,8 +3,7 @@
 парсит страницу с расписанием студентов и выбирает href на экселл-файл
 c расписанием
 """
-
-from typing import List, Dict
+import typing
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -13,21 +12,11 @@ TIME_TABLE_URL = "https://knute.edu.ua/blog/read/?pid=1038&uk"
 def get_timetable_page():
     """возвращает Respond страницы расписания
     """
-    return requests.get(TIME_TABLE_URL)
+    resp = requests.get(TIME_TABLE_URL)
+    
+    return resp if resp.status_code == 200 else None
 
 def get_parsed_timetable():
     pass
+    return None
     
-
-
-def main():
-    pass
-    
-    # читать страницу расписания
-    
-    # переходім на начало секціі распісанія
-    
-    # находим таблицу бакадавров и выдкляем шапку
-    
-    
-    # извлечение url эксел файлов 
--- Config/test_make_config.py
+++ Config/test_make_config.py
@@ -1,3 +1,5 @@
+
+# import pytest
 from make_config import \
     get_timetable_page
 
@@ -6,5 +8,4 @@ def test_get_timetable_page():
     assert get_timetable_page().status_code == 200
 
 # def test_get_parsed_timetable():
-#     assert 
 #     pass
