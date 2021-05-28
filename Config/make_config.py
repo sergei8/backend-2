f3f53dfa74b613f376160370e704c649773589f6
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2021-05-28 21:37:40 +0300

    read tables

--- Config/make_config.py
+++ Config/make_config.py
@@ -3,20 +3,69 @@
 парсит страницу с расписанием студентов и выбирает href на экселл-файл
 c расписанием
 """
-import typing
+from typing import Union, Any, Tuple
 import requests
 from bs4 import BeautifulSoup as bs
 
 TIME_TABLE_URL = "https://knute.edu.ua/blog/read/?pid=1038&uk"
+DENNA_FORMA_TABLE_NAME = "ДЕННА ФОРМА НАВЧАННЯ"
 
-def get_timetable_page():
+def get_timetable_page(url: str):
     """возвращает Respond страницы расписания
     """
-    resp = requests.get(TIME_TABLE_URL)
+    resp = requests.get(url)
     
     return resp if resp.status_code == 200 else None
 
-def get_parsed_timetable():
-    pass
-    return None
+def get_parsed_timetable(html_page) -> Union[bs, None]:
+    """строит и возвращает soup-объект из страницы расписаний
+
+    Args:
+        html_page (str): объект страницы
+
+    Returns:
+         распарсеный soup или None
+    """
+    try:
+        parsed_page = bs(html_page.content, features='html.parser')
+        if not isinstance(parsed_page, bs):
+            return None
+
+    except:
+        return None
+
+    return parsed_page
     
+def extract_time_tables(parsed_page) -> Tuple[bs, bs]: 
+    # выделить начало секции расписания денной формы
+    denna_forma:bs = parsed_page.find("strong", text=DENNA_FORMA_TABLE_NAME)
+    
+    # таблиц бакалавров (через одну в DOM)
+    table_bakalavr:bs = denna_forma.parent.next_sibling.next_sibling
+
+    # таблица магистров
+    table_magistr:bs = table_bakalavr.next_sibling.next_sibling
+
+    return (table_bakalavr, table_magistr)
+
+def main():
+    
+    # читать страницу расписания
+    time_table_page = get_timetable_page(TIME_TABLE_URL)
+    if not time_table_page:
+        print("ошибка чтения url расписания")
+        exit(1)
+    
+    # распарсить страницу расписаний
+    parsed_page = get_parsed_timetable(time_table_page)
+    if not parsed_page:
+        print("ошибка парсинга страницы расписания")
+        exit(1)
+
+    # выделить таблицу и загрузить ее в pandas
+    denna_forma = extract_time_tables(parsed_page)
+    # print(denna_forma)
+    
+
+if __name__ == '__main__':
+    main()
--- Config/test_make_config.py
+++ Config/test_make_config.py
@@ -1,11 +1,46 @@
 
-# import pytest
+import pytest
+from bs4 import BeautifulSoup as bs
+# from re import sub
+
+# import sys
+# sys.path.append('./Config')
+
 from make_config import \
-    get_timetable_page
+    extract_time_tables
+    
 
-def test_get_timetable_page():
-    assert get_timetable_page() != None
-    assert get_timetable_page().status_code == 200
+@pytest.fixture
+def soup_page():
+    test_html = """
+    <html>
+        <body>
+            <div>
+                <strong>ДЕННА ФОРМА НАВЧАННЯ</strong>
+            </div>
+            <div>...</div>
+            <table>
+                <tbody>
+                    <tr><td>ФИТ-бакалавры</td></tr>
+                    <tr><td><a href='/file=бакалавры'>...</a></td></tr>
+                </tbody>
+            </table>
+            <div>...</div>
+            <table>
+                <tbody>
+                    <tr><td>ФИТ-магистры</td></tr>
+                    <tr><td><a href='/file=магистры'>...</a></td></tr>
+                </tbody>
+            </table>
+        </body>
+    </html>
+    """
+    test_html = test_html.replace('\n','')
+    test_html = test_html.replace('  ','')
+    soup = bs(test_html, features='html.parser')
 
-# def test_get_parsed_timetable():
-#     pass
+    return soup
+
+def test_extract_time_tables(soup_page):
+    assert isinstance(extract_time_tables(soup_page), tuple) == True
+    pass
